d66e8210533c8fb98c991cd59a8598b23d281ccd
Weipeng He <heweipeng@gmail.com>
2014-10-20 12:13:15 +0200

    Add LGPL

--- notghmm/notghmm.h
+++ notghmm/notghmm.h
@@ -1,8 +1,22 @@
-// notghmm.h
-// Header file for basic data structure and functions.
-//
-// Author : Weipeng He <heweipeng@gmail.com>
-// Copyright (c) 2014, All rights reserved.
+/* NotGHMM
+ * Copyright (c) 2014 Weipeng He <heweipeng@gmail.com>
+ * notghmm.h : Header file for basic data structure and functions.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #ifndef NOTGHMM_H_
 #define NOTGHMM_H_
